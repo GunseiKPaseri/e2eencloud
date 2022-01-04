9b42a52ea10dfbed9824845d4f7be22989005239
GunseiKPaseri <GunseiKPaseri@gmail.com>
2022-01-04 18:20:06 +0900

    パスワード判定・確認処理の追加

--- webcli/src/features/auth/signup.tsx
+++ webcli/src/features/auth/signup.tsx
@@ -1,11 +1,33 @@
 import React, { useState, ReactElement } from 'react';
 import { useAppDispatch } from '../../app/hooks';
 import { signupAsync } from './authSlice';
+import zxcvbn from 'zxcvbn';
+import PasswordChecker from './PasswordChecker';
+
+const correctEmailaddr = /^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/;
 
 export const Signup:React.FC = ():ReactElement => {
+  const [confirmMode, setConfirmMode] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const dispatch = useAppDispatch();
+  // メールアドレスが適切なものか判定
+  const isGoodMailAddress = correctEmailaddr.test(email);
+  // 現在のパスワードをスコア化
+  const res = zxcvbn(password, [email]);
+  const passwordScore:0|1|2|3|4 = password.length < 8 ? 0 : res.score;
+
+  // 入力が受け入れられるものなら確認画面に遷移
+  const confirm = () => {
+    setConfirmMode(true);
+  };
+  // 拒絶するときは初期画面に戻す
+  const cancel = () => {
+    setConfirmPassword("");
+    setConfirmMode(false);
+  };
+  // 確認が取れたらサインアップ
   const signup = async () => {
     await dispatch(signupAsync({email, password}));
   };
@@ -15,9 +37,28 @@ export const Signup:React.FC = ():ReactElement => {
       <h2>サインアップ</h2>
       <p>パスワードは絶対忘れないようにしてください</p>
       <form>
-        <label>ユーザ名：<input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} /></label>
-        <label>パスワード：<input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} /></label>
-        <button onClick={signup}>送信</button>
+      {
+        (confirmMode ?
+          <>
+            <label>パスワード（確認のため再入力）：<input type="password" value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)} /></label><br/>
+            <button onClick={signup}disabled={password!==confirmPassword}>送信</button><br/>
+            <button onClick={cancel}>取り消し</button>
+          </>
+          :
+          <>
+            <label>メールアドレス：<input type="email" value={email} onChange={(e)=>{
+              setEmail(e.target.value);
+            }} /></label><br />
+            {!isGoodMailAddress ? <><span style={{color: 'red'}}>正しくないメールアドレスです</span><br /></> : <></> }
+            <label>パスワード：<input type="password" value={password} onChange={(e)=>{
+              setPassword(e.target.value);
+            }} /></label><br />
+            <PasswordChecker score={passwordScore} /><br />
+            <button onClick={confirm} disabled={!isGoodMailAddress || passwordScore<2}>確認</button>
+          </>
+          )
+      }
+        
       </form>
     </article>
   )
