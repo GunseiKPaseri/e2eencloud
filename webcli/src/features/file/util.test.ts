0d488ec624ca94474cac85e3b5e5a26cc2e21cc6
GunseiKPaseri <GunseiKPaseri@gmail.com>
2022-02-04 21:34:48 +0900

    ファイル名テストの追加

--- webcli/src/features/file/util.test.ts
+++ webcli/src/features/file/util.test.ts
@@ -1,9 +1,23 @@
 import {
-  genUUID
+  genUUID, getAddingNumberFileName, getSafeName
 } from './utils'
 
 describe('#genUUID', () => {
-  test('-を含まない', () => {
+  test('\'-\'を含まない', () => {
     expect(genUUID().indexOf('-')).toBe(-1)
+  })
+})
+
+describe('#getSafeName', () => {
+  test('安全な文字列に変換される', () => {
+    expect(getSafeName([
+      '\\/:*?<>|',
+      'hoge.fuga.piyo',
+      'hoge.fuga.piyo'
+    ], ['hoge.fuga.piyo'])).toEqual([
+      '＼／：＊？＜＞｜',
+      'hoge.fuga (1).piyo',
+      'hoge.fuga (2).piyo']
+    )
   })
 })
