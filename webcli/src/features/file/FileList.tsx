531cc48b9641a69c165eb18f3c3a7b1f3cadf418
GunseiKPaseri <GunseiKPaseri@gmail.com>
2022-02-08 18:33:31 +0900

    詳細ビューの追加

--- webcli/src/features/file/FileList.tsx
+++ webcli/src/features/file/FileList.tsx
@@ -7,6 +7,8 @@ import ListItemText from '@mui/material/ListItemText'
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
 import ToggleButton from '@mui/material/ToggleButton'
 
+import { DataGrid, GridRowsProp } from '@mui/x-data-grid'
+
 import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile'
 import FolderIcon from '@mui/icons-material/Folder'
 import ViewListIcon from '@mui/icons-material/ViewList'
@@ -49,9 +51,11 @@ const FileListListFile = (props: {targetFile: FileNodeFile, onSelectFile: (id: s
   )
 }
 
-const FileListList = () => {
+export const FileList = () => {
+  const [viewStyle, setViewStyle] = useState<'list' | 'detaillist'>('list')
+
   const dispatch = useAppDispatch()
-  const fileState = useAppSelector<FileState>(state => state.file)
+  const { fileTable, activeFileGroup } = useAppSelector<FileState>(state => state.file)
 
   const onSelectFolder = useCallback((id: string) => {
     dispatch(changeActiveDir({ id }))
@@ -62,34 +66,50 @@ const FileListList = () => {
   }, [dispatch])
 
   return (
-    <List>
-      {
-        fileState.activeFileGroup
-          ? fileState.activeFileGroup.files.map((x) => {
-            const target = fileState.fileTable[x]
-            assertNonFileNodeDiff(target)
-            if (target.type === 'folder') {
-              return (<FileListListFolder key={target.id} targetFolder={target} onSelectFolder={onSelectFolder} />)
-            } else {
-              return (<FileListListFile key={target.id} targetFile={target} onSelectFile={onSelectFile} />)
-            }
-          })
-          : <></>
-      }
-    </List>
-  )
-}
-
-export const FileList = () => {
-  const [viewStyle, setViewStyle] = useState<'list' | 'detaillist'>('list')
-
-  return (
     <>
       <ToggleButtonGroup value={viewStyle} exclusive onChange={(e, nextView) => setViewStyle(nextView)}>
         <ToggleButton value='list'><ViewListIcon /></ToggleButton>
         <ToggleButton value='detaillist'><ViewHeadlineIcon /></ToggleButton>
       </ToggleButtonGroup>
-      {viewStyle === 'list' ? <FileListList /> : <></>}
+      {
+        activeFileGroup
+          ? viewStyle === 'list'
+            ? (
+              <List>
+                {
+                  activeFileGroup.files.map((x) => {
+                    const target = fileTable[x]
+                    assertNonFileNodeDiff(target)
+                    if (target.type === 'folder') {
+                      return (<FileListListFolder key={target.id} targetFolder={target} onSelectFolder={onSelectFolder} />)
+                    } else {
+                      return (<FileListListFile key={target.id} targetFile={target} onSelectFile={onSelectFile} />)
+                    }
+                  })
+                }
+              </List>
+              )
+            : <div style={{ height: 300, width: '100%' }}>
+                <DataGrid
+                  rows={
+                    activeFileGroup.files.map<GridRowsProp[number]>(x => {
+                      const target = fileTable[x]
+                      assertNonFileNodeDiff(target)
+                      return target.type === 'folder'
+                        ? { id: target.id, name: target.name, mime: '', size: '', tags: '' }
+                        : { id: target.id, name: target.name, mime: target.mime, size: target.size, tags: target.tag.join(',') }
+                    })
+                  }
+                  columns={[
+                    { field: 'id', hide: true },
+                    { field: 'name', headerName: '名前', width: 200 },
+                    { field: 'mime', headerName: 'MIMEタイプ', width: 200 },
+                    { field: 'size', headerName: 'ファイルサイズ', width: 200 },
+                    { field: 'tags', headerName: 'タグ', width: 200 }
+                  ]} />
+              </div>
+          : <></>
+      }
     </>
   )
 }
