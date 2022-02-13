4f991803a0673afe3aee84b343f1e6f1b2b5666c
GunseiKPaseri <GunseiKPaseri@gmail.com>
2022-02-13 17:31:04 +0900

    ファイルビューの位置関係を整理

--- webcli/src/App.tsx
+++ webcli/src/App.tsx
@@ -1,33 +1,28 @@
-import { Login } from './features/auth/login'
-import { FileDropZone } from './features/file/filedrop'
 
 import { SideMenu } from './components/sidemenu/SideMenu'
 
 import Box from '@mui/material/Box'
+import { useTheme } from '@mui/material/styles'
+import { FileExplorer } from './components/fileexplorer/FileExplorer'
 
 function App () {
+  const theme = useTheme()
   return (
     <Box sx={{ display: 'flex' }}>
       <SideMenu/>
       <Box
         component="main"
         sx={{
+          paddingTop: 8,
           backgroundColor: (theme) =>
             theme.palette.mode === 'light'
               ? theme.palette.grey[100]
               : theme.palette.grey[900],
           flexGrow: 1,
-          height: '100vh',
-          overflow: 'auto'
+          height: '100vh'
         }}
       >
-        <div className="App">
-          <header className="App-header">
-            <h1>APIテスト用</h1>
-            <Login />
-            <FileDropZone />
-          </header>
-        </div>
+        <FileExplorer />
       </Box>
     </Box>
   )
---
+++ webcli/src/components/fileexplorer/FileExplorer.tsx
@@ -0,0 +1,41 @@
+
+import Grid from '@mui/material/Grid'
+import Paper from '@mui/material/Paper'
+import { useTheme } from '@mui/material/styles'
+import { Login } from '../../features/auth/login'
+import { FileTreeViewer } from '../../features/file/FileTreeViewer'
+import { FileList } from '../../features/file/FileList'
+import { Viewer } from '../../features/file/Viewer'
+import { AddFolder } from '../../features/file/AddFolder'
+import { DiffTree } from '../../features/file/DiffTree'
+
+export const FileExplorer = () => {
+  const theme = useTheme()
+  return ( 
+    <div style={{
+      display: 'grid',
+      gridAutoFlow: 'column',
+      alignItems: 'stretch',
+      gridTemplateColumns: '300px 1fr',
+      gridTemplateRows: '1fr 1fr',
+      padding: theme.spacing(1),
+      gridGap: theme.spacing(1),
+      height: `calc(100vh - ${theme.spacing(8)})`
+    }}>
+      <Paper sx={{ overflow: 'scroll', gridRow: '1/3' }}>
+        <FileTreeViewer />
+        <Login />
+      </Paper>
+      <Paper sx={{ overflowY: 'scroll'}}>
+        <AddFolder />
+        <FileList />
+      </Paper>
+      <Paper sx={{ overflowY: 'scroll'}}>
+        <h2>ファイルビュー</h2>
+        <Viewer />
+        <h2>ファイルヒストリー</h2>
+        <DiffTree />
+      </Paper>
+    </div>
+  )
+}
