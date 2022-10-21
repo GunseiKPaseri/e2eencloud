2045cb2de772841c9a4a1da81d6132d8942fcc6d
GunseiKPaseri <GunseiKPaseri@gmail.com>
2022-10-22 00:35:46 +0900

    update vite&vitest

--- webcli/vite.config.ts
+++ webcli/vite.config.ts
@@ -1,9 +1,5 @@
-/// <reference types="vitest" />
-
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import reactJsx from 'vite-react-jsx'
-import manifestSRI from 'vite-plugin-manifest-sri'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,6 +13,9 @@ export default defineConfig({
     'process.browser': true,
     'process.env': {}
   },
+  server: {
+    port: 3000
+  },
   test: {
     globals: true,
     environment: 'happy-dom',
@@ -26,7 +25,6 @@ export default defineConfig({
     }
   },
   plugins: [
-    manifestSRI({ algorithms: ['sha384', 'sha512'] }),
-    reactJsx()
+    react()
   ]
 })
