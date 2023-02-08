b171ef171787e4cfc546b650905b3453ba721423
GunseiKPaseri <GunseiKPaseri@gmail.com>
2023-02-08 17:15:51 +0900

    add vite swc plugin

--- webcli/vite.config.ts
+++ webcli/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -27,6 +27,6 @@ export default defineConfig({
     }
   },
   plugins: [
-    react()
+    react({ plugins: [["@swc/plugin-styled-components", {}]] })
   ]
 })
