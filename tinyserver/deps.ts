a4e3d3c94daac6a92328b8c25b25cefbf78d8369
GunseiKPaseri <GunseiKPaseri@gmail.com>
2023-07-20 01:37:40 +0900

    deno package update

--- tinyserver/deps.ts
+++ tinyserver/deps.ts
@@ -1,20 +1,20 @@
 export {
   decode as base642ByteArray,
   encode as byteArray2base64,
-} from 'https://deno.land/std@0.177.0/encoding/base64.ts';
+} from 'https://deno.land/std@0.194.0/encoding/base64.ts';
 export {
   decode as base64Url2ByteArray,
   encode as byteArray2base64Url,
-} from 'https://deno.land/std@0.177.0/encoding/base64url.ts';
+} from 'https://deno.land/std@0.194.0/encoding/base64url.ts';
 
-export { bold, yellow } from 'https://deno.land/std@0.177.0/fmt/colors.ts';
-export { v4 } from 'https://deno.land/std@0.177.0/uuid/mod.ts';
+export { bold, yellow } from 'https://deno.land/std@0.194.0/fmt/colors.ts';
+export { v4 } from 'https://deno.land/std@0.194.0/uuid/mod.ts';
 
-export type { IsExact } from 'https://deno.land/std@0.177.0/testing/types.ts';
-export { assertType } from 'https://deno.land/std@0.177.0/testing/types.ts';
+export type { IsExact } from 'https://deno.land/std@0.194.0/testing/types.ts';
+export { assertType } from 'https://deno.land/std@0.194.0/testing/types.ts';
 
 // load dotenv
-import 'https://deno.land/std@0.177.0/dotenv/load.ts';
+import 'https://deno.land/std@0.194.0/dotenv/load.ts';
 export * as ENV from './env.ts';
 
 // load dependent package
@@ -28,21 +28,21 @@ export {
 } from 'https://deno.land/x/oak@v11.1.0/mod.ts';
 
 export { Session as oakSession } from 'https://deno.land/x/oak_sessions@v4.1.0/mod.ts';
-import OakSessionStore from 'https://deno.land/x/oak_sessions@v4.1.0/src/stores/Store.ts';
+import OakSessionStore from 'https://deno.land/x/oak_sessions@v4.1.9/src/stores/Store.ts';
 export type { OakSessionStore };
-export type { SessionData } from 'https://deno.land/x/oak_sessions@v4.1.0/src/Session.ts';
+export type { SessionData } from 'https://deno.land/x/oak_sessions@v4.1.9/src/Session.ts';
 
 export { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts';
 
-export { SMTPClient } from 'https://deno.land/x/denomailer@1.5.3/mod.ts';
+export { SMTPClient } from 'https://deno.land/x/denomailer@1.6.0/mod.ts';
 
-import * as z from 'https://deno.land/x/zod@v3.20.2/mod.ts';
+import * as z from 'https://deno.land/x/zod@v3.21.4/mod.ts';
 export { z };
 
-export { expect } from 'https://deno.land/x/expect@v0.2.10/mod.ts';
+export { expect } from 'https://deno.land/x/expect@v0.4.0/mod.ts';
 
-// @deno-types="https://deno.land/x/otpauth@v9.0.2/dist/otpauth.d.ts"
-export * as OTPAuth from 'https://deno.land/x/otpauth@v9.0.2/dist/otpauth.esm.js';
+// @deno-types="https://deno.land/x/otpauth@v9.1.3/dist/otpauth.d.ts"
+export * as OTPAuth from 'https://deno.land/x/otpauth@v9.1.3/dist/otpauth.esm.js';
 
 // @deno-types="npm:@types/ua-parser-js@0.7.36"
 import uaparser from 'npm:ua-parser-js@1.0.33';
@@ -53,11 +53,11 @@ export { S3Bucket } from 'https://deno.land/x/s3@0.5.0/mod.ts';
 import compareAsc from 'https://deno.land/x/date_fns@v2.22.1/compareAsc/index.ts';
 export { compareAsc };
 
-export { Fido2AssertionResult, Fido2Lib } from 'https://deno.land/x/fido2@3.3.5/dist/main.js';
+export { Fido2AssertionResult, Fido2Lib } from 'https://deno.land/x/fido2@3.4.1/dist/main.js';
 
-import UUIDshort from 'npm:short-uuid';
+import UUIDshort from 'npm:short-uuid@4.2.2';
 export { UUIDshort };
-export { uuidv7 } from 'npm:uuidv7';
+export { uuidv7 } from 'npm:uuidv7@0.5.3';
 
 import * as bcrypt from 'https://deno.land/x/bcrypt@v0.4.1/mod.ts';
 export { bcrypt };
@@ -71,6 +71,3 @@ export { bs58 };
 
 // for
 import _prisma from 'prisma';
-
-import * as trpc from 'npm:@trpc/server@10.14.1';
-export { trpc };
