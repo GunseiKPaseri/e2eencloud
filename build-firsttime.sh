608cd98eb96a0677d44f83d4f6007ee0f38cba9b
GunseiKPaseri <GunseiKPaseri@gmail.com>
2023-07-26 01:18:34 +0900

    fix: for devcontainer

--- build-firsttime.sh
+++ build-firsttime.sh
@@ -3,27 +3,30 @@ set -x
 cd `dirname $0`
 
 # remove container・volume
-docker compose -f ./docker-compose.yml -f ./docker-compose.build.yml down -v
+docker compose --project-name e2eencloud -f $(pwd)/docker-compose.yml -f $(pwd)/docker-compose.build.yml down -v
 
-# load .env
+# load prisma gen
 docker compose \
+    --project-name e2eencloud \
     -f ./docker-compose.yml -f ./docker-compose.build.yml \
     run --rm apiserver \
     bash -c "cd /e2eencloud/tinyserver && deno task prisma:gen"
-docker compose -f ./docker-compose.yml -f ./docker-compose.build.yml down
+docker compose -f $(pwd)/docker-compose.yml -f $(pwd)/docker-compose.build.yml down
 
-# # migrate
-docker compose -f ./docker-compose.yml -f ./docker-compose.build.yml up -d
+# migrate
+docker compose -f $(pwd)/docker-compose.yml -f $(pwd)/docker-compose.build.yml up -d
 docker compose \
-    -f ./docker-compose.yml -f ./docker-compose.build.yml \
+    --project-name e2eencloud \
+    -f $(pwd)/docker-compose.yml -f $(pwd)/docker-compose.build.yml \
     exec apiserver \
     bash -c "cd /e2eencloud/tinyserver && sleep 20 && deno task prisma:mgdev"
-docker compose -f ./docker-compose.yml -f ./docker-compose.build.yml down
+docker compose -f $(pwd)/docker-compose.yml -f $(pwd)/docker-compose.build.yml down
 
-# # seed
-docker compose -f ./docker-compose.yml -f ./docker-compose.build.yml up -d
+# seed
+docker compose -f $(pwd)/docker-compose.yml -f $(pwd)/docker-compose.build.yml up -d
 docker compose \
-    -f ./docker-compose.yml -f ./docker-compose.build.yml \
+    --project-name e2eencloud \
+    -f $(pwd)/docker-compose.yml -f $(pwd)/docker-compose.build.yml \
     exec apiserver \
     bash -c "cd /e2eencloud/tinyserver && sleep 20 && deno task prisma:seed"
 
