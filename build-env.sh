b173caeec15f3f4458cae42ec7a47e7978c5fcde
GunseiKPaseri <GunseiKPaseri@gmail.com>
2023-08-03 18:07:59 +0900

    update: deno

--- build-env.sh
+++ build-env.sh
@@ -4,5 +4,5 @@
 docker run --name e2ee_initialize \
     --rm \
     --mount "type=bind,source=$(pwd),target=/e2eencloud" \
-    denoland/deno:1.35.2 \
+    denoland/deno:1.35.3 \
     bash -c "cd /e2eencloud/tinyserver && deno task init"
