7d2221999076930fa31cab5754dace0f87acb47e
GunseiKPaseri <GunseiKPaseri@gmail.com>
2023-08-05 13:48:52 +0000

    add: Detail component

--- webcli/src/pages/components/FileExplorer.tsx
+++ webcli/src/pages/components/FileExplorer.tsx
@@ -1,12 +1,12 @@
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-import Login from '~/features/auth/components/login/Login';
 import FileTreeViewer from '~/features/file/components/FileTreeViewer';
 import FileList from '~/features/file/components/FileList';
 import Viewer from '~/features/file/components/Viewer';
 import { DiffTree } from '~/features/file/components/molecule/DiffTree';
 import TagButton from '~/features/file/components/atom/TagButton';
 import Exclctrl from '~/features/exclctrl/Exclctrl';
+import Detail from '~/features/file/components/molecule/Detail';
 
 export default function FileExplorer() {
   return (
@@ -25,13 +25,13 @@ export default function FileExplorer() {
         <Exclctrl />
         <FileTreeViewer />
         <TagButton tag="bin" />
-        <Login />
       </Paper>
       <Paper sx={{ overflowY: 'scroll', padding: 1 }}>
         <FileList />
       </Paper>
       <Paper sx={{ overflowY: 'scroll', padding: 1 }}>
         <Viewer />
+        <Detail />
         <DiffTree />
       </Paper>
     </Box>
